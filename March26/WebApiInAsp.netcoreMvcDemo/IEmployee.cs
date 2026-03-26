70b3ec633756fb6e19ba1ecbedd43ea0346462e4
vivek-kumar-srivastava <vivekisonline31@gmail.com>
2026-03-26 23:49:17 +0530

    March26

---
+++ March26/WebApiInAsp.netcoreMvcDemo/IEmployee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiInAsp.netcoreMvcDemo.Models;
+
+namespace WebApiInAsp.netcoreMvcDemo
+{
+    public interface IEmployee
+    {
+
+            Task<List<Employee>> GetAllEmployeesAsync(int pageNumber,int pageSize);
+            Task<Employee?> GetEmployeeByIdAsync(int id);
+            Task<Employee> AddEmployeeAsync(Employee employee,IFormFile image);
+            Task<Employee?> UpdateEmployeeAsync(Employee employee,IFormFile? image);
+            Task<Employee?> DeleteEmployeeAsync(int id);
+        
+        
+    }
+}
