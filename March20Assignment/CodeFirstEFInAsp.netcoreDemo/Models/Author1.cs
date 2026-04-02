f518c01acfc1721ec0bf333f52973229cc826ecc
vivek-kumar-srivastava <vivekisonline31@gmail.com>
2026-04-02 14:13:20 +0530

    week wise assignments

--- March20Assignment/CodeFirstEFInAsp.netcoreDemo/Models/Author1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeFirstEFInAsp.netcoreDemo.Models
-{
-    public class Author1
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public IList<Course1>Courses { get; set; }
-    }
-}
