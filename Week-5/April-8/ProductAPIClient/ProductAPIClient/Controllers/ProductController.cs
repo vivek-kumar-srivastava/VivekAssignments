39d4063bee6ea65c3707cf97fd80854a97514279
vivek-kumar-srivastava <vivekisonline31@gmail.com>
2026-04-09 10:10:31 +0530

    April-8

---
+++ Week-5/April-8/ProductAPIClient/ProductAPIClient/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPIClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ProductController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ApiBaseUrl = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+
+    }
+}
---
+++ Week-5/April-8/ProductAPIClient/ProductAPIClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductAPIClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
