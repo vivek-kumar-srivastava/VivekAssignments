2bc620df784f25279d7725a86fbabe437fb25fc5
vivek-kumar-srivastava <vivekisonline31@gmail.com>
2026-04-16 09:33:39 +0530

    april 15 assignment

---
+++ Week-6/April-15/batch2vs/batch2vs/Program.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using System.Text;
+{
+    
+}
+namespace batch2vs
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string tenantId = "";
+            string clientId = "";
+            string clientSecret = "";
+
+
+          //  var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+           // string vaultUrl = "";
+            string keyName = "batch2vs";
+
+
+            var keyClient = new KeyClient(new Uri(vaultUrl), credential);
+
+
+
+            KeyVaultKey key;
+
+            key = await keyClient.GetKeyAsync(keyName);
+
+            string originalText = "Sensitive order data for CloudXeus Technology Services";
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(originalText);
+
+            var cryptoClient = new CryptographyClient(key.Id, credential);
+
+            EncryptResult encryptResult = await cryptoClient.EncryptAsync(
+                EncryptionAlgorithm.RsaOaep,
+                plaintextBytes);
+
+            Console.WriteLine("Encrypted text (Base64):");
+            Console.WriteLine(Convert.ToBase64String(encryptResult.Ciphertext));
+
+            DecryptResult decryptResult = await cryptoClient.DecryptAsync(
+                EncryptionAlgorithm.RsaOaep,
+                encryptResult.Ciphertext);
+
+            string decryptedText = Encoding.UTF8.GetString(decryptResult.Plaintext);
+
+            Console.WriteLine("\nDecrypted text:");
+            Console.WriteLine(decryptedText);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
